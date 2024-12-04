6f55da94be15b4257bfd207f5e5d36322a4ed384
Sadia Afrin <afrinzzxx@gmail.com>
2024-12-04 21:18:06 +0600

    User model,error handler,authRoute,userCtrl started and db connect done

---
+++ controller/userCtrl.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const createUser = asyncHandler(async (req, res) => {
+  const email = req.body.email;
+  const findUser = await User.findOne({email: email});
+  if (!findUser) {
+    //create a new user
+    const newUser = User.create(req.body);
+    res.json(newUser);
+  } else {
+    //user already exists
+      throw new Error("User Already Exists");
+    
+  }
+});
+
+module.exports={createUser};
