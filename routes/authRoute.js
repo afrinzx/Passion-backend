90967e8f3e34a7eddfe88846e6d86457f8df90c9
Sadia Afrin <afrinzzxx@gmail.com>
2024-12-06 10:37:40 +0600

    Get single user,alluser,delete a user,update a user,admin authorized,update user using admin logged in with token

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,20 @@
-const express=require("express");
-const { createUser, loginUserCtrl } = require("../controller/userCtrl");
-const router=express.Router();
-router.post("/register",createUser);
-router.post("/login",loginUserCtrl);
-module.exports=router;
+const express = require("express");
+const {
+  createUser,
+  loginUserCtrl,
+  getallUsers,
+  getaUser,
+  deleteaUser,
+  updatedUser,
+} = require("../controller/userCtrl");
+const {authMiddleware,isAdmin} = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/register", createUser);
+router.post("/login", loginUserCtrl);
+router.get("/all-users", getallUsers);
+router.get("/:id",authMiddleware,isAdmin, getaUser);
+router.delete("/:id", deleteaUser);
+router.put("/edit-user",authMiddleware, updatedUser);
+
+module.exports = router;
