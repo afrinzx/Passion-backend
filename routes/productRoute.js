3771cedeeb0e93681f8cfd1c73198e474de1e583
Sadia Afrin <afrinzzxx@gmail.com>
2024-12-09 21:40:11 +0600

    Update product,delete,auth to create update delete is completed

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,18 @@
-const express=require("express");
-const {createProduct,getaProduct,getAllProduct}=require("../controller/productCtrl");
-const router=express.Router();
+const express = require("express");
+const {
+  createProduct,
+  getaProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controller/productCtrl");
+const {isAdmin,authMiddleware}=require("../middlewares/authMiddleware")
+const router = express.Router();
 
-router.post("/",createProduct);
-router.get("/:id",getaProduct);
-router.get("/",getAllProduct);
+router.post("/",authMiddleware,isAdmin, createProduct);
+router.get("/:id", getaProduct);
+router.put("/:id",authMiddleware,isAdmin, updateProduct);
+router.delete("/:id",authMiddleware,isAdmin, deleteProduct);
+router.get("/", getAllProduct);
 
-module.exports=router;
+module.exports = router;
