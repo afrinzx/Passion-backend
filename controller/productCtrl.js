73452fdacce5288a9969fe222e301ea06117af5a
Sadia Afrin <afrinzzxx@gmail.com>
2024-12-10 20:10:00 +0600

    Fliter,Sort,Limit,Pagination is completed

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -50,14 +50,45 @@ const getaProduct = asyncHandler(async (req, res) => {
 });
 
 const getAllProduct = asyncHandler(async (req, res) => {
-  //const { id } = req.params;
-  //console.log(req.query);
   try {
-    const getAllProducts = await Product.find(
-      //req.query
-      { brand: req.query.brand, category: req.query.category }
-    );
-    res.json(getAllProducts);
+    //flitering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    console.log(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    //sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+    //limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query == query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("This Page does not exists");
+    }
+    console.log(page, limit, skip);
+
+    const product = await query;
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
